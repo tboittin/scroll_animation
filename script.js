fba02964381abafc45fe44db19e0214070a3873d
Thomas Boittin <tboittin.pro@gmail.com>
2021-01-24 08:31:20 +0100

    js animation

---
+++ script.js
@@ -0,0 +1,19 @@
+const boxes = document.querySelectorAll('.box')
+
+const checkBoxes = () => {
+  const triggerBottom = (window.innerHeight * 4) / 5
+
+  boxes.forEach((box) => {
+    const boxTop = box.getBoundingClientRect().top
+
+    if (boxTop < triggerBottom) {
+      box.classList.add('show')
+    } else {
+      box.classList.remove('show')
+    }
+  })
+}
+
+window.addEventListener('scroll', checkBoxes)
+
+checkBoxes()
